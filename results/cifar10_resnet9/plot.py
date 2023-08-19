112c7c40b1ad69c13692e2354dc31a4d4625d9f1
rafayel01 <rafaelveziryan@gmail.com>
2023-08-19 19:32:19 +0400

    added test file

--- results/cifar10_resnet9/plot.py
+++ results/cifar10_resnet9/plot.py
@@ -1,19 +1,29 @@
 import pickle
 import matplotlib.pyplot as plt
 
-with open('/home/rafayel/cnn_exp/results/cifar10_resnet9/ResNet9_par_5_list.pkl', 'rb') as f:
+with open('/home/rafayel/cnn_exp/results/cifar10_resnet9/ResNet9_par_5_all_list.pkl', 'rb') as f:
     data = pickle.load(f)
 
 
 # print(data[0])
 
 length: int = len(data[0]['val_acc'])
+print(length)
 
 def plot(x: list[float], y: list[float]) -> None:
     plt.plot(x, y)
-    plt.show()
 
 
+y = [None for _ in range(6)]
 for i in range(6):
-    y = [data[i]['val_acc'][j] * 100 for j in range(100)]
-    plot(range(length), data[i]['val_acc'])
+    y[i] = [data[i]['val_acc'][j] * 100 for j in range(101)]
+    print(max(data[i]['val_acc']))
+
+
+plot(range(length), y[0])
+plot(range(length), y[1])
+plot(range(length), y[2])
+plot(range(length), y[3])
+plot(range(length), y[4])
+plot(range(length), y[5])
+plt.show()
