7d00731bb65e5c93e48ce088966b2926f1e5547e
rafayel01 <rafaelveziryan@gmail.com>
2024-01-01 23:16:00 +0400

    rearrangements of files

--- codes/cifar10/resnet_cifar10_v4.py
+++ codes/cifar10/resnet_cifar10_v4.py
@@ -887,14 +887,14 @@ model_cable_eq_2 = ResNet18_with_cable_eq_2(img_channels=3, num_layers=18, block
 model_cable_eq_3 = ResNet18_with_cable_eq_3(img_channels=3, num_layers=18, block=BasicBlock, num_classes=10).to(device)
 model_cable_eq_4 = ResNet18_with_cable_eq_4(img_channels=3, num_layers=18, block=BasicBlock, num_classes=10).to(device)
 model_cable_eq_5 = ResNet18_with_cable_eq_5(img_channels=3, num_layers=18, block=BasicBlock, num_classes=10).to(device)
-models = (model_original, ) #model_cable_eq_2, model_cable_eq_3, model_cable_eq_4, model_cable_eq_5)
+models = (model_original, model_cable_eq_2, model_cable_eq_3, model_cable_eq_4, model_cable_eq_5)
 
 
 for model in models:
     print(f"{model._get_name()}: {parameter_count(model)}")
 
 
-
+'''
 for model in models:
     valid_loss_min = np.Inf
     n_epochs = 100
@@ -919,7 +919,7 @@ for model in models:
 
 
 # Printing images, before and after my layers
-# '''
+# 
 # imgs = next(iter(trainloader))
 # images, labels = imgs[0][:4], imgs[1][:4]
 # # show images
