b79543cb8e3d17241ec5f28798d0419e302c3aec
rafayel01 <rafaelveziryan@gmail.com>
2024-01-01 23:22:15 +0400

    modified files

--- results/plot.py
+++ results/plot.py
@@ -1,35 +1,41 @@
 import pickle
 import matplotlib.pyplot as plt
 
-models = ("ResNet18_with_cable_eq_bn", "ResNet18_with_cable_eq_2_bn", "ResNet18_with_cable_eq_3_bn", "ResNet18_with_cable_eq_4_bn", "ResNet18_with_cable_eq_5_bn")
+models = ("ResNet18_bn", "ResNet18_with_cable_eq_bn", "ResNet18_with_cable_eq_2_bn", "ResNet18_with_cable_eq_3_bn") #, "ResNet18_with_cable_eq_4_bn", "ResNet18_with_cable_eq_5_bn")
+models2 = ("ResNet18_with_cable_eq_bn", "ResNet18_with_cable_eq_2_bn", "ResNet18_with_cable_eq_3_bn")
+
 data =[]
+data2 = []
 for model in models:
-    with open(f'/home/rafayel/cnn_exp/results/cifar10_lr_0.5_blocks/{model}.pkl', 'rb') as f:
+    with open(f'/PATH_OF_FILE/{model}.pkl', 'rb') as f:
         data.append(pickle.load(f))
 
+
+for model in models2:
+    with open(f'/home/rafayel/cnn_exp/results/stl10_lr_0.5_blocks_final/{model}.pkl', 'rb') as f:
+        data.append(pickle.load(f))
+
+leg = ("Original", "CabEq 1", "CabEq 2", "CabEq 3", "CabEqBl 1", "CabEqBl 2", "CabEqBl 3")
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+
 length: int = len(data[-1]['test_acc'])
+all_models = models + models2
 for i in range(len(data)):
-    print(max(data[i]["test_acc"]))
-    plt.plot(range(length), data[i]["test_acc"])
+    print(f'Best Test Acc ({all_models[i]}): {max(data[i]["test_acc"])}')
+    ax1.plot(range(length), data[i]["test_acc"], label=leg[i])
+
+ax1.set_xlabel("Number of Epochs")
+ax1.set_ylabel("Accuracy")
+ax1.legend(fontsize=13)
+
+for i in range(len(data)):
+    ax2.plot(range(length), data[i]["test_loss"], label=leg[i])
+
+ax2.set_xlabel("Number of Epochs")
+ax2.set_ylabel("Loss")
 
 
+plt.legend(fontsize=13)
 plt.show()
 
-#print(data)
-
-# length: int = len(data['val_acc'])
-# # print(length)
-
-# def plot(x: list[float], y: list[float]) -> None:
-#     plt.plot(x, y)
-
-
-# # y = [None for _ in range(6)]
-# # for i in range(6):
-# #     y[i] = [data[i]['val_acc'][j] * 100 for j in range(101)]
-# #     print(data[i]['val_acc'][-1])
-
-# print(data['val_acc'])
-# print(max(data['val_acc']))
-# plot(range(length), data['val_acc'])
-# plt.show()
